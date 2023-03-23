67b0ceca8e3e0da520cc1b5b186a221d11f1d8b8
Ranjeet Kumar <ranjeet8082@gmail.com>
2023-03-23 15:52:21 +0530

    tablr with its stories and mdx

--- src/components/Tablr.stories.jsx
+++ src/components/Tablr.stories.jsx
@@ -3,11 +3,24 @@ import { Tablr } from "./Tablr";
 export default {
   title: "components/Tablr",
   component: Tablr,
-  args: {
-    userName: "Abc",
-  },
 };
 
-export const Basic = (args) => {
+const Template = (args) => {
   return <Tablr {...args}></Tablr>;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  rows: [
+    ["This", "is", "just", "a", "test"],
+    ["This", "is", "also", "a", "test"],
+    ["Just", "a", "little", "more", "data"],
+    ["Row", "number", "four", "right", "here"],
+  ],
+  headers: ["Col 1", "Col 2", "Col 3", "Col 4", "Col 5"],
+};
+
+Default.argTypes = {
+  rows: { control: "" },
+  headers: { control: "" },
 };
